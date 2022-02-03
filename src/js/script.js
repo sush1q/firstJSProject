ba938d9412734c8e8e71de8691134c86721a4f93
Павел Сироткин <pavelsirotkin@MacBook-Air.local>
2022-02-03 20:00:18 +0300

    commit

--- src/js/script.js
+++ src/js/script.js
@@ -1,6 +1,27 @@
 'use strict';
 
 let money, time;
+let startBtn = document.querySelector('#start'),
+    budgetValue = document.querySelector(".budget-value"),
+    dayBudgetValue = document.querySelector(".daybudget-value"),
+    levelValue = document.querySelector(".level-value"),
+    expensesValue = document.querySelector(".expenses-value"),
+    optionalExpensesValue = document.querySelector(".optionalexpenses-value"),
+    incomeValue = document.querySelector(".income-value"),
+    yearSavingsValue = document.querySelector(".yearsavings-value"),
+    monthSavingsValue = document.querySelector(".monthsavings-value"),
+    expensesItem = document.querySelector(".expenses-item"),
+    expensesItemBtn = document.getElementsByTagName("expenses-item-btn"),
+    optionalExpensesBtn = document.getElementsByTagName("optionalexpenses-btn"),
+    countBudgetBtn = document.getElementsByTagName("count-budget-btn"),
+    optionalExpensesItem = document.querySelector(".optionalexpenses-item"),
+    chooseIncome = document.querySelector(".choose-income"),
+    savingsCheckbox = document.querySelector("#savings"),
+    chooseSum = document.querySelector(".choose-sum"),
+    choosePercent = document.querySelector(".choose-percent"),
+    yearValue = document.querySelector(".year-value"),
+    monthValue = document.querySelector(".month-value"),
+    dayValue = document.querySelector(".day-value");
 
 
 function start(){
@@ -90,6 +111,10 @@ for (let key in appData){
 
 
 
+
+
+    
+    
 
 
 
